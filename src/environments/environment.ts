26499722fd4964ab17891c4597f07ebd4f2f9a64
adrian zelada torrez <zelada_torrez@hotmail.com>
2019-11-04 23:07:54 -0400

    first

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyBRPlquR-qhlbOk_htuC64gpiAf0jkmy0U",
+    authDomain: "scorching-fire-4444.firebaseapp.com",
+    databaseURL: "https://scorching-fire-4444.firebaseio.com",
+    projectId: "scorching-fire-4444",
+    storageBucket: "scorching-fire-4444.appspot.com",
+    messagingSenderId: "378160403959",
+    appId: "1:378160403959:web:42c8db91c7fc6c48210af2"
+  }
 };
 
 /*
